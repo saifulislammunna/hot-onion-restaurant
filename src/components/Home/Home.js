032ed181fc90462cdf1adcc2c845fa5d59cb21b4
saifulislammunna <saifulmunna28@gmail.com>
2021-10-15 15:26:14 +0600

    food-added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
-
+import Food from '../Food/Food';
+import './Home.css'
 const Home = () => {
 
     const [items, setItems] = useState([]);
     useEffect( () => {
         fetch(`./items.JSON`)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => setItems(data));
     },[])
     return (
-        <div>
-            <h2>This is home</h2>
+        <div className="items">
+        <div className="items-container">
+        {
+                items.map(item => <Food 
+                item = {item}
+                key = {item.img}
+                >
+
+                </Food>)
+            }
+        </div>
         </div>
     );
 };
